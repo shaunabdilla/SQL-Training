f22ff4df7e7b3bd8d15a86b3ba9f49c7ec7de349
Shaun Abdilla <53258781+shaunabdilla@users.noreply.github.com>
2019-09-13 16:05:06 +0200

    Finished Intermediate Half 1
    Aggregation, Coun, Sum, Min/Max, AVG, Group By, Having, Distinct

--- Intermediate Query.sql
+++ Intermediate Query.sql
@@ -40,3 +40,72 @@ WHERE high IS NOT NULL
 -- Write a query that calculates the average daily trade volume for Apple stock.
 SELECT AVG(volume)
 FROM tutorial.aapl_historical_stock_price
+
+-- Calculate the total number of shares traded each month. Order your results chronologically.
+ SELECT year,
+       month,
+       SUM(volume) AS volume_sum
+FROM tutorial.aapl_historical_stock_price
+GROUP BY year, month
+ORDER BY year ASC, month ASC
+
+-- Write a query to calculate the average daily price change in Apple stock, grouped by year.
+SELECT year,
+     AVG(close-open) AS price_change_avg
+FROM tutorial.aapl_historical_stock_price
+GROUP BY year
+ORDER BY 1
+
+-- Write a query that calculates the lowest and highest prices that Apple stock achieved each month.
+
+SELECT year,
+      month,
+      MAX(high) as max_high,
+      MIN(low) as min_low
+FROM tutorial.aapl_historical_stock_price
+GROUP BY 1,2 
+ORDER BY 1,2 
+
+SELECT year,
+      month,
+      MAX(high) AS month_high
+  FROM tutorial.aapl_historical_stock_price
+GROUP BY year, month
+HAVING MAX(high) > 400
+ORDER BY year, month
+
+SELECT DISTINCT month
+  FROM tutorial.aapl_historical_stock_price
+
+SELECT DISTINCT year, month
+  FROM tutorial.aapl_historical_stock_price
+
+-- -- Write a query that returns the unique values in the year column, in chronological order.
+SELECT DISTINCT year
+FROM tutorial.aapl_historical_stock_price
+ORDER BY year ASC
+
+SELECT COUNT(DISTINCT month) AS unique_months
+  FROM tutorial.aapl_historical_stock_price
+
+SELECT month,
+      AVG(volume) AS avg_trade_volume
+  FROM tutorial.aapl_historical_stock_price
+GROUP BY month
+ORDER BY 2 DESC
+
+-- Write a query that counts the number of unique values in the month column for each year.
+
+SELECT COUNT(DISTINCT month) AS unique_months,
+      year
+FROM tutorial.aapl_historical_stock_price
+GROUP BY year
+
+/* Write a query that separately counts the number of unique values 
+in the month column and the number of unique values in the `year` column.*/
+
+SELECT COUNT(DISTINCT month) AS unique_month,
+      COUNT(DISTINCT year) AS unique_year
+FROM tutorial.aapl_historical_stock_price
+
+
